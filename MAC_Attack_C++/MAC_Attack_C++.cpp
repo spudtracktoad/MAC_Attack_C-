7efe8568989a1ca20af365e69be6a8c5ab580c91
Jared Smith <spudtracktoad@gmail.com>
2020-09-18 22:26:58 -0400

    still testing

--- MAC_Attack_C++/MAC_Attack_C++.cpp
+++ MAC_Attack_C++/MAC_Attack_C++.cpp
@@ -8,29 +8,30 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    const string orgMessage = "No one has completed Project #3 so give them all a 0";
+    /*const string orgMessage = "No one has completed Project #3 so give them all /*a 0";
     uint32_t orgHash[5] = { 0xac94e7cf, 0x99456fbf, 0xe5e7aa79, 0xe94d8905, 0x7889b67e };
-    string addMessage = "P.S. Except for jsmit210, go ahead and give him the full points.";
+    string addMessage = "P.S. Except for jsmit210, go ahead and give him the full points.";*/
 
-    /*string orgMessage = "abc";
-    string testMessage = "abcd";
+    string orgMessage = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    string testMessage = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    uint32_t testHash[5] = { 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0 };
     uint32_t orgHash[5] = { 0xA9993E36, 0x4706816A, 0xBA3E2571, 0x7850C26C, 0x9CD0D89D };
-    string addMessage = "d";*/
-    
-    Custom_Sha1 corSha1;/*
+    string addMessage = "d"; 
+  
+    Custom_Sha1 corSha1;
     corSha1.update(testMessage);
     string corHash = corSha1.final();
-    cout << "The SHA-1 of \"" << testMessage << "\" is: " << corHash << endl;*/
+    cout << "The SHA-1 of \"" << testMessage << "\" is: " << corHash << endl;
 
-    Custom_Sha1 mySha;
-    mySha.update(orgMessage);
-    string myHash = mySha.final();
+    //Custom_Sha1 mySha;
+    //mySha.update(orgMessage);
+    //string myHash = mySha.final();
     Custom_Sha1 checksum;
-    checksum.update(addMessage, orgHash, 1024);
+    checksum.update(orgMessage, testHash, 0);
     string hash = checksum.final();
 
-    cout << "The SHA-1 of \"" << orgMessage << "\" is: " << myHash << endl;
-    cout << "The Mal SHA-1 of \"" << addMessage << "\" is: " << hash << endl;
+    //cout << "The SHA-1 of \"" << orgMessage << "\" is: " << myHash << endl;
+    cout << "The Mal SHA-1 of \"" << orgMessage << "\" is: " << hash << endl;
 
     return 0;
 
